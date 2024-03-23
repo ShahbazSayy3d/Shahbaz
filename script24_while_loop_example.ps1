3adad6ca0d912ec32b40ae8dac1d9669c1482f2b
ShahbazSayy3d <141297247+ShahbazSayy3d@users.noreply.github.com>
2024-03-24 00:21:27 +0530

    Add files via upload

---
+++ script24_while_loop_example.ps1
@@ -0,0 +1,7 @@
+﻿$array = @("item1", "item2", "item3")
+$counter = 0;
+
+while($counter -lt $array.Length){
+   $array[$counter]
+   $counter += 1
+}
---
+++ script25_do_while_loop_example.ps1
@@ -0,0 +1,9 @@
+﻿$array = @("item1", "item2", "item3")
+$counter = 0;
+cls
+
+do{
+  $array[$counter]
+  $counter += 1
+}
+while($counter -lt $array.Length)
