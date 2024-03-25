affd1d471feebff2b4ca1c235e3f4ead0e9006f1
ShahbazSayy3d <141297247+ShahbazSayy3d@users.noreply.github.com>
2024-03-25 16:49:52 +0530

    Add files via upload

---
+++ 02_list_of_exe_files_in_windows_folder.ps1
@@ -0,0 +1,17 @@
+﻿#creating a list variable to store the path in it
+$list = Get-ChildItem -path "C:\Windows\*.exe"
+$list
+
+#list displayed using format-table
+#Format-Table -InputObject $list
+
+#list displayed using format-list
+Format-List -InputObject $list
+
+#list displayed using format-wide
+Format-Wide -InputObject $list
+
+<#
+exe is the filename extension that denotes a file as being executable – a computer program.
+It is often called an executable file
+#>
